22bbd6d43ef4aa665f51d06bf1014310834ea6c6
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-11-02 17:46:42 -0300

    refatoracao

--- heapSort.java
+++ heapSort.java
@@ -2,53 +2,75 @@
 public class heapSort {
 
     public static void main(String[] args) {
-        int[] arr = { 55, 10, 33, 22, 62, 30, 14, 80, 77, 35 };
+        int[] arr = { 55, 10, 33, 22, 11, 25 };
 
-        heapSort(arr);
+        heapSortAlgorithm(arr);
+
+        // heapSortAlgorithmInterative(arr, arr.length);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    public static void heapSort(int[] vetor) {
-
-        // Cria o heap maximo a partir do vetor desordenado (heapify)
-        for (int i = vetor.length / 2 - 1; i >= 0; i--) {
-            heapify(vetor, vetor.length, i);
+    public static void heapSortAlgorithm(int vetor[]) {
+        int n = vetor.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heap(vetor, n, i);
         }
-
-        // Ordena o vetor (heap sort)
-        for (int i = vetor.length - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             int temp = vetor[0];
             vetor[0] = vetor[i];
             vetor[i] = temp;
-            heapify(vetor, i, 0);
+            heap(vetor, i, 0);
         }
     }
 
-    public static void heapify(int[] vetor, int tamanhoVetor, int i) {
-        int raiz = i;
-        int filho1 = 2 * i + 1;
-        int filho2 = 2 * i + 2;
+    private static void heap(int vetor[], int n, int i) {
+        int largest = i;
+        int esquerda = 2 * i + 1;
+        int direita = 2 * i + 2;
+        if (esquerda < n && vetor[esquerda] > vetor[largest])
+            largest = esquerda;
+        if (direita < n && vetor[direita] > vetor[largest])
+            largest = direita;
+        if (largest != i) {
+            int swap = vetor[i];
+            vetor[i] = vetor[largest];
+            vetor[largest] = swap;
+            heap(vetor, n, largest);
+        }
+    }
 
-        // Se o filho da esquerda for maior que a raiz
-        if (filho1 < tamanhoVetor && vetor[filho1] > vetor[raiz]) {
-            raiz = filho1;
+    public static void heapSortAlgorithmInterative(int vetor[], int n) {
+        int i = n / 2;
+        int pai, filho, t;
+        while (true) {
+            if (i > 0) {
+                i--;
+                t = vetor[i];
+            } else {
+                n--;
+                if (n == 0)
+                    return;
+                t = vetor[n];
+                vetor[n] = vetor[0];
+            }
+            pai = i;
+            filho = i * 2 + 1;
+            while (filho < n) {
+                if ((filho + 1 < n) && (vetor[filho + 1] > vetor[filho]))
+                    filho++;
+                if (vetor[filho] > t) {
+                    vetor[pai] = vetor[filho];
+                    pai = filho;
+                    filho = pai * 2 + 1;
+                } else {
+                    break;
+                }
+            }
+            vetor[pai] = t;
         }
 
-        // Se o filho da direita for maior que a raiz
-        if (filho2 < tamanhoVetor && vetor[filho2] > vetor[raiz]) {
-            raiz = filho2;
-        }
-
-        // Se a raiz não for a maior então troca com o maior filho e chama a função
-        // novamente para o filho que foi trocado
-        if (raiz != i) {
-            int temp = vetor[i];
-            vetor[i] = vetor[raiz];
-            vetor[raiz] = temp;
-            heapify(vetor, tamanhoVetor, raiz);
-        }
     }
 }
